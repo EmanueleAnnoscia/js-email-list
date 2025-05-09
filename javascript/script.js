d596eb846b512e8bf6081a25fa89964954def8db
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-05-09 14:08:55 +0200

    creazione del pulsante refresh

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 //prelevo gli elementi
 const emailList = document.querySelector("#email-list")
-
+const btnElem = document.querySelector(".btn")
 
 
 // creo le 10 email
@@ -16,15 +16,44 @@ const emailList = document.querySelector("#email-list")
 // stampo le 10 mail in una lista
   
     
-     for(i = 1 ; i<10; i++){
-     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
-        //  console.log(resp)
-        const email = resp.data.response;
-        emailList.innerHTML += `<li>${email}</li>`
-     });
+    //  for(i = 1 ; i<10; i++){
+    //  axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
+    //     //  console.log(resp)
+    //     const email = resp.data.response;
+    //     emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
+    //  });
      
      
+
+    //  }
+
+    // funzione di generazione email e di refresh delle ultime
+
+     function generateEmails(){
+         for(i = 1 ; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
+            //  console.log(resp)
+            const email = resp.data.response;
+            emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
+            });
+        }
 
      }
 
-    //  
+    //  richiamo la funzione
+    generateEmails();
+
+    btnElem.addEventListener("click",refresh);
+
+    function refresh(){
+        emailList.innerHTML="";
+        for(i = 1 ; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
+            //  console.log(resp)
+            const email = resp.data.response;
+            emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
+            });
+        }
+
+    }
+
