8084fc0322929b9eff51146a40480a21aeeb0b73
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-05-09 15:32:45 +0200

    fix minori

--- javascript/script.js
+++ javascript/script.js
@@ -33,27 +33,82 @@ const btnElem = document.querySelector(".btn")
          for(let i = 0 ; i<10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
             //  console.log(resp)
-            const email = resp.data.response;
-            emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
-            });
-        }
+             const email = resp.data.response;
+             emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
+             });
+         }
+
+      }
+
+     //  richiamo la funzione
+     generateEmails();
+
+     // aggiungo al bottone l'evento
+     btnElem.addEventListener("click",refresh);
+ // funzione di refresh
+     function refresh(){
+         emailList.innerHTML="";
+         for(i = 1 ; i<10; i++){
+             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
+//             //  console.log(resp)
+             const email = resp.data.response;
+             emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
+             });
+         }
 
      }
 
-    //  richiamo la funzione
-    generateEmails();
+//--------------------------------------sezione di prova implementazione caricamenti simultanei-------------------------------------------------------------------------
 
-    btnElem.addEventListener("click",refresh);
+// function generateEmails() {
+//   emailList.innerHTML = "<li>Caricamento in corso...</li>";
 
-    function refresh(){
-        emailList.innerHTML="";
-        for(i = 1 ; i<10; i++){
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
-            //  console.log(resp)
-            const email = resp.data.response;
-            emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`
-            });
-        }
+//   let emailArr = [];
+//   let counter = 0;
 
-    }
+//   for (let i = 0; i < 10; i++) {
+//     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+//       .then((resp) => {
+//         const email = resp.data.response;
+//         emailArr.push(email);
+//         counter++;
 
+//         if (counter === 10) {
+//           //una volta che le ho ricevute le svuoto e le stampo
+//           emailList.innerHTML = "";
+//           emailArr.forEach((email) => {
+//             emailList.innerHTML += `<li class="list-group-item"><a>${email}</a></li>`;
+//           });
+//         }
+//       });
+//   }
+// }
+
+// // Chiamo la funzione una volta all'avvio
+// generateEmails();
+
+// // evento click sul bottone pe ril refresh
+// btnElem.addEventListener("click", generateEmails);
+
+
+
+    // prova di creazione contemporanea 
+
+     let emailArr=[]
+     let counter = 0; //quando il counter arriverà a 10 allora stamperà tutte le mail contemporaneamente
+
+     for (let i=0; i<10; i++){
+         axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
+// //          console.log(resp)
+          emailArr.push(resp.data.response)
+        console.log(emailArr)
+         counter ++; 
+        
+         if(counter = 10){
+             emailList.innerHTML += emailArr[i]
+         }
+
+       });
+
+     
+     }
